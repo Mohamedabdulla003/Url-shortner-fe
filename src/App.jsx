a0dc0a10f7964c07ae203bbb2367c81989371752
Mohamedabdulla003 <abdullamohamed406@gmail.com>
2024-07-10 14:03:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import axios from "axios";
+import NavBar from "./componenets/navbar";
+import Main from "./componenets/main";
+import UrlShortener from "./componenets/urlShortener";
+import Statistics from "./componenets/statistics";
+import Cta from "./componenets/cta";
+import Footer from "./componenets/footer";
+
+function App() {
+  const [urls, setUrls] = useState([]);
+  const handleClick = async (url) => {
+    try {
+      // Define the headers
+      const headers = {
+        "Content-Type": "text/plain",
+        apikey: "AtJpfMCXaU61UYkNWnCYtZKCPvh3OQkV",
+      };
+
+      const shortenUrl = "https://api.apilayer.com/short_url/hash";
+      const data = url;
+
+      // const response = await axios(
+      //   `https://api.shrtco.de/v2/shorten?url=${url}`
+      // );
+      const response = await axios.post(shortenUrl, data, { headers });
+
+      setUrls((prevUrls) => {
+        if (prevUrls.length === 0) {
+          return [
+            {
+              org: url,
+              short: response.data.short_url,
+              copied: false,
+            },
+          ];
+        } else {
+          return [
+            {
+              org: url,
+              short: response.data.result.full_short_link,
+              copied: false,
+            },
+            ...prevUrls,
+          ];
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleCopyText = (id) => {
+    setUrls((prevUrls) =>
+      prevUrls.map((url, index) => {
+        return index === id
+          ? { ...url, copied: true }
+          : { ...url, copied: false };
+      })
+    );
+  };
+  console.log(urls);
+  return (
+    <div>
+      <div className="container relative my-6 px-3 lg:mx-auto lg:px-[6rem]">
+        <NavBar />
+        <Main />
+        <div className="relative">
+          <UrlShortener handleClick={handleClick} />
+        </div>
+      </div>
+      <div>
+        <Statistics urlData={urls} handleCopyText={handleCopyText} />
+        <Cta />
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
